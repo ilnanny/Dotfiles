9537b7466d86c6ea96716537881020a7db3ca439
ilnanny <ilnannyhack@gmail.com>
2020-03-12 01:13:23 +0100

    update bash scripts

--- Void/etc/bash/bashrc.d/utente.sh
+++ Void/etc/bash/bashrc.d/utente.sh
@@ -1,47 +1,48 @@
-#================================================
-#================================================
-#   O.S.      : Gnu Linux                       =
-#   Author    : Cristian Pozzessere   = ilnanny =
-#   D.A.Page  : http://ilnanny.deviantart.com   =
-#   Github    : https://github.com/ilnanny      =
-#================================================
-#================================================
+#  ___________________________  Bashrc  ___________________________
 #
-#  ________________________________    Direttive    ________________________________
+# _____ Interattiva
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+# _____ Alias
+if [ -f ~/.bash_aliases ]; then
+. ~/.bash_aliases
+fi
+# _____ Profili
+for SH in /etc/profile.d/*.sh; do
+       . $SH
+done
+# _____ Alias .sh
+for sh in /etc/bash/bashrc.d/* ; do
+	[[ -r ${sh} ]] && source "${sh}"
+done
+#
+#  ___________________________  Direttive  ___________________________
 export GPG_TTY=$(tty)
 export TERM="xterm"
 export EDITOR="nano"
-export BROWSER="firefox-bin"
+export BROWSER="firefox"
 export WEBBROWSER="firefox"
-export FILEMANAGER='thunar"
+export FILEMANAGER"thunar"
 export LC_TYPE="it_IT.UTF-8"
-export LANG="it_IT.UTF-8'
+export LANG="it_IT.UTF-8"
 export XDG_CONFIG_HOME="$HOME/.config"
 export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 export PATH="$HOME/bin:$PATH"
-export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:$PATH"
+export XKBMAP="it"
 export WIKI="$HOME/Wiki:$PATH"
 export SCRIPT="$HOME/Scripts:$PATH"
 export DOTFILE="$HOME/Dotfiles:$PATH"
 export LOCALBIN="$HOME/.local/bin:$PATH"
 export HOMEBIN="$HOME/bin:$PATH"
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:/usr/lib/pkgconfig:$PATH"
 #
 export MANWIDTH=90
 export HISTSIZE=10000
 export HISTIGNORE="q:f:v"
-shopt -s direxpand
-shopt -s checkhash
-shopt -s sourcepath
-shopt -s expand_aliases
-shopt -s autocd cdspell
-shopt -s extglob dotglob
-shopt -s no_empty_cmd_completion
-shopt -s autocd cdable_vars cdspell
-shopt -s cmdhist histappend histreedit histverify
-[[ $DISPLAY ]] && shopt -s checkwinsize
 #
-#
-#  ________________________________    Colori Ansi     ________________________________
+#  ___________________________  Colori Ansi  ___________________________
 case "$TERM" in
     xterm-color|*-256color) color_prompt=yes;;
 esac
@@ -62,7 +63,7 @@ export LESS_TERMCAP_so=$'\e[01;44;33m'
 export LESS_TERMCAP_ue=$'\e[0m'
 export LESS_TERMCAP_us=$'\e[01;32m'
 #
-#  ________________________________     Git     ________________________________
+#  ___________________________  Git  ___________________________
 #
 SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
@@ -76,11 +77,11 @@ GIT_THEME_PROMPT_SUFFIX="${green}|"
 
 CONDAENV_THEME_PROMPT_SUFFIX="|"
 
-#  ________________________________     Shell  Utente     ________________________________
+#  ___________________________  Shell  Utente  ___________________________
 #
 #
 PS1="┌─[\`if [ \$? = 0 ]; then echo \[\e[32m\]✔\[\e[0m\]; else echo \[\e[31m\]✘\[\e[0m\]; fi\`]───[\[\e[01;49;39m\]\u\[\e[00m\]\[\e[01;49;39m\]@\H\[\e[00m\]]───[\[\e[1;49;34m\]\W\[\e[0m\]]───[\[\e[1;49;39m\]\$(ls | wc -l) files, \$(ls -lah | grep -m 1 total | sed 's/total //')\[\e[0m\]]\n└───▶ "
 #
 #
-# _____________________________________  Fine  _______________________________________
+# ___________________________   Fine  _____________________________
 
